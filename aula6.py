3465937c436e9e7bc9d22e490cff1898d6cbece2
wellsant <wellington_santanna@outlook.com>
2024-04-27 09:22:51 -0300

    first commit

---
+++ aula6.py
@@ -0,0 +1,5 @@
+# Project Descriptions
+
+# Explore Python from basics to advanced concepts. Dive into syntax, data types, control flow, object-oriented programming, data manipulation, and web development with Flask or Django. 
+# Elevate your Python skills with hands-on examples and exercises.
+
