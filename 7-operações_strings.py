fd465b4222eb534957511827433b8b66f16ccbc5
wellsant <wellington_santanna@outlook.com>
2024-07-10 06:25:41 -0300

    Second commit

--- 7-operações_strings.py
+++ 7-operações_strings.py
@@ -3,7 +3,7 @@ Fifa 23 é um jogo de futebol
 desenvolvido pela EA Sports
 e que possibilita jogar localmente ou online.
         '''
-    
+        
 gameName = 'Fifa'
 gameVersion = ' 23'
 # 1 - Operação de Concatenação de String 
